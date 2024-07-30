8583bd8d1454a29281c01b71cc090487126e3e10
lucas <lucas.belini@escola.pr.gov.br>
2024-07-30 08:25:58 -0300

    joia

---
+++ projeto_novo_zipe-2/gerenciamentofornecedor.cs
@@ -0,0 +1,44 @@
+public class gerenciamentofornecedor{
+
+String criterioselecao;
+int id;
+String avaliacaodesempenho;
+string analisar;
+
+///*****************************************************************************************///
+                                /// </summary>classe Set Email <summary>///  
+public void Setemail(string email){
+    this.criterioselecao=criterioselecao;
+}
+
+///*****************************************************************************************///
+                               /// </summary> classe ID <summary>///             
+public void Setid(){
+this.id = 1000;
+}
+
+///*****************************************************************************************///
+                              /// </summary>classe Get Email <summary>///  
+public string Getemail(string email){
+    return email;
+}
+
+///*****************************************************************************************///
+
+public void Setavaliacaodesempenho(string avaliacaodesempenho){
+
+this.avaliacaodesempenho = avaliacaodesempenho;
+
+}
+///******************************************************************************************///
+
+public void Getavaliacaodesempenho(string avaliacaodesempenho){
+ 
+}
+public void Getanalisar(string analisar){
+    return analisar;
+}
+public void Setanalisar(){
+    this.analisar=analisar;
+}
+}
--- projeto_novo_zipe-2/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/projeto_novo_zipe-2.AssemblyInfo.cs
+++ projeto_novo_zipe-2/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/projeto_novo_zipe-2.AssemblyInfo.cs
@@ -10,10 +10,14 @@
 using System;
 using System.Reflection;
 
+[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.PackageName", "com.companyname.projeto_novo_zipe-2")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.PublisherName", "User Name")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.Name", "projeto_novo_zipe-2")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.Version", "1.0.0.1")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("projeto_novo_zipe-2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5f00503e8692ada6a4e57de07490d7beeefa70f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+1691253a29994d62dd62c76d26d410ed9588099e")]
 [assembly: System.Reflection.AssemblyProductAttribute("projeto_novo_zipe-2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projeto_novo_zipe-2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
